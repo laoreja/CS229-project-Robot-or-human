9442784e8c92f64cb54fbf878998b9febee1a79e
Shuyang Shi <shuyang790@gmail.com>
2017-11-20 10:34:34 -0800

    fix: SVM probability=True

--- predictors/SVMRBF.py
+++ predictors/SVMRBF.py
@@ -3,7 +3,7 @@ from commons import *
 
 name = "SVMRBF"
 
-svc = svm.SVC(kernel='rbf')
+svc = svm.SVC(kernel='rbf', probability=True)
 X_train, y_train = prepareTrainData()
 evaluateClassifier(svc, X_train, y_train, name)
 printSubmission(svc, X_train, y_train, name)
